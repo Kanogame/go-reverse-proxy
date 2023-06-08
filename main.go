41451ecc15db5e4079bc1abcb680c3b52faec972
kanogame <wasdqert1222@gmail.com>
2023-06-08 16:24:28 +0700

    parsing config pt1

--- main.go
+++ main.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	utils "main/Utils"
 	"os"
-	"text/scanner"
+	"strings"
 )
 
 func main() {
-	config := ReadConfigFile("./config")
+	config := ReadConfigFile("./config.txt")
+	ParseConfig(config)
 }
 
-func ReadConfigFile(path string) string[] {
+func ReadConfigFile(path string) []string {
 	file, err := os.Open(path)
-	if utils.HandleUserError(err) {
+	if !utils.HandleUserError(err) {
 		utils.PrintError("No config specified, no config in default directory")
 	}
 	defer file.Close()
@@ -25,4 +25,23 @@ func ReadConfigFile(path string) string[] {
 		lines = append(lines, scanner.Text())
 	}
 	return lines
+}
+
+func ParseConfig(config []string) {
+	var ParsedConfig []string
+	//get work area
+	for i := 0; i < len(config); i++ {
+		if strings.Contains(config[i], "http") {
+			var end int
+			for j := 0; j < len(config); j++ {
+				if strings.Contains(config[(len(config)-1)-j], "}") {
+					end = len(config) - 1 - j
+					break
+				}
+			}
+			ParsedConfig = config[i+1 : end-1]
+			break
+		}
+	}
+
 }
