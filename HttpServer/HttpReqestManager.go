d52b6621b6b8329bd489f3260ebaf30620c88daf
kanogame <wasdqert1222@gmail.com>
2023-06-14 11:10:36 +0700

    http server rewrite pt1

--- HttpServer/HttpReqestManager.go
+++ HttpServer/HttpReqestManager.go
@@ -4,31 +4,16 @@ import (
 	"fmt"
 	utils "main/Utils"
 	"net/http"
-	"strconv"
-
-	"github.com/rs/cors"
 )
 
-func StartHttpServer(port int, locations *utils.Locations) {
-	var c = cors.New(cors.Options{
-		AllowedOrigins: []string{ /*all*/ },
-	})
-
-	responseHandler := func(w http.ResponseWriter, r *http.Request) {
-		return HttpHandler(w, r, locations)
-	}
-
-	handler := http.HandlerFunc(responseHandler())
+func StartHttpServer(port string, locations *utils.Locations) {
+	locationHandler(locations)
+	http.ListenAndServe(":"+port, nil)
 	fmt.Println("Http Server Started and listening at: ", port)
-	http.ListenAndServe(":"+strconv.Itoa(port), c.Handler(handler))
 }
 
-func (locations *utils.Locations) HttpHandler(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path
-	if r.Method == "GET" {
-		//TODO
-		//http.ServeFile(w, r, path)
-	} else if r.Method == "POST" {
-		//TODO
+func locationHandler(locations *utils.Locations) {
+	for _, staticServer := range locations.proxy {
+
 	}
 }
