5eda3a2bfa6dacf7b26fdb8407a8aa84f6f4a823
kanogame <wasdqert1222@gmail.com>
2023-06-18 13:46:56 +0700

    proxy reg done

--- HttpServer/HttpReqestManager.go
+++ HttpServer/HttpReqestManager.go
@@ -9,12 +9,17 @@ import (
 func StartHttpServer(port string, locations *utils.Locations) {
 	locationHandler(locations)
 	fmt.Println("Http Server Started and listening at: ", port)
-	http.ListenAndServe(":"+port, nil)
+	err := http.ListenAndServe(":"+port, nil)
+	utils.HandleAppError(err)
 }
 
 func locationHandler(locations *utils.Locations) {
 	for _, staticServer := range *locations.Static {
 		r := &StaticLocations{&staticServer}
 		go http.HandleFunc(staticServer.WebPath, r.HandleStatic())
+	}
+	for _, proxyServer := range *locations.Proxy {
+		r := &ProxyLocations{&proxyServer}
+		go http.HandleFunc(proxyServer.WebPath, r.HandleProxy())
 	}
 }
