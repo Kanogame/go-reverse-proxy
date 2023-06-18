05e83013033bbb576e7dd9da680233ef7d382c2e
kanogame <wasdqert1222@gmail.com>
2023-06-18 14:12:11 +0700

    test done!

--- Test/HttpServer_test.go
+++ Test/HttpServer_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"io/ioutil"
 	"log"
 	httpserver "main/HttpServer"
 	utils "main/Utils"
@@ -25,5 +26,32 @@ func TestHandleStatic(t *testing.T) {
 	}
 	if res.StatusCode != 200 {
 		t.Errorf("expected 200 got %v", res.StatusCode)
+	}
+}
+
+func TestHandleProxy(t *testing.T) {
+	test := &httpserver.ProxyLocations{
+		ProxyLocations: &utils.ProxyLocations{
+			WebPath:  "/",
+			EndPoint: "http://127.0.0.1:12312/"},
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("<div>hello world</div>")) })
+	t.Log("Test Server listening at: 12312")
+	go http.ListenAndServe(":12312", nil)
+
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	w := httptest.NewRecorder()
+	test.HandleProxy().ServeHTTP(w, req)
+
+	res := w.Result()
+	if res.StatusCode != 200 {
+		t.Errorf("expected 200 got %v", res.StatusCode)
+	}
+	defer res.Body.Close()
+	data, err := ioutil.ReadAll(res.Body)
+	utils.HandleAppError(err)
+	if string(data) != "<div>hello world</div>" {
+		t.Errorf("expected <div>hello world</div> got %v", string(data))
 	}
 }
