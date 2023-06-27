c50b28f43f92abaaedad6203535aa2de8670e894
kanogame <wasdqert1222@gmail.com>
2023-06-27 19:35:17 +0700

    re-created cli proxy app for testing

--- Proxy-App/main.go
+++ Proxy-App/main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", serveFolder)
 	fmt.Println("Http Server Started and listening at: 12312")
-	http.ListenAndServe(":12312", nil)
+	http.ListenAndServe(":"+os.Args[2], nil)
 }
 
 func serveFolder(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./static/")
+	http.ServeFile(w, r, os.Args[1])
 }
